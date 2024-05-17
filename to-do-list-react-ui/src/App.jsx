aa15f33182207f3ee6c0e9499ee155d13bf1f0e8
Sergio-Arean <Sergio.Arean.-ND@disney.com>
2024-05-17 15:43:55 -0300

    project scaffolding & 1st component

---
+++ to-do-list-react-ui/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section className="content">
+      <h1 className="title">To Do List</h1>
+
+      <div className="list-container">
+        
+      </div>
+    </section>
+  )
+
+}
+
+export default App
